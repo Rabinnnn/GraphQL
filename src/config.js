621bb15728e98ba8345ce69cb1e08c6ffae67aa2
rotieno <rabinyitzahck@gmail.com>
2025-07-03 22:11:04 +0300

    chore: create configuration constants

---
+++ src/config.js
@@ -0,0 +1,5 @@
+
+// constants
+export const DOMAIN = "learn.zone01kisumu.ke";
+export const SIGNIN_ENDPOINT = `https://${DOMAIN}/api/auth/signin`;
+export const GRAPHQL_ENDPOINT = `https://${DOMAIN}/api/graphql-engine/v1/graphql`;
